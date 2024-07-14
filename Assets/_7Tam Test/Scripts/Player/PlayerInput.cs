2a488dd1c12a8c025a689eae40a8500858739686
Hassan <hassanalmonajjed@gmail.com>
2024-07-14 21:25:50 +0300

    Fix: Joystic lagging

--- Assets/_7Tam Test/Scripts/Player/PlayerInput.cs
+++ Assets/_7Tam Test/Scripts/Player/PlayerInput.cs
@@ -6,7 +6,28 @@ namespace SevenTamTest.Player
     public class PlayerInput : MonoBehaviour
     {
         public Vector2 Movement { get; private set; }
+        private _7tamTest _playerControl;
+        private InputAction _move;
 
-        public void OnMove(InputValue input) => Movement = input.Get<Vector2>();
+        private void Awake()
+        {
+            _playerControl = new _7tamTest();
+            _move = _playerControl.Player.Move;
+        }
+
+        private void OnEnable()
+        {
+            _playerControl.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerControl.Disable();
+        }
+
+        private void Update()
+        {
+            Movement = _move.ReadValue<Vector2>();
+        }
     }
 }
